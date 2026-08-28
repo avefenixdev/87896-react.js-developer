ecef27fd65650a3dac564fab3d8483b63a7d49f3
Ave Fenix Dev <avefenixdev@gmail.com>
2026-08-28 10:00:48 -0300

    Trabajando con los componentes Link y NavLink

--- clase-04/01-react-router/src/components/Navbar.jsx
+++ clase-04/01-react-router/src/components/Navbar.jsx
@@ -1,7 +1,12 @@
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav>
+        <a href="/">Inicio</a>
+        <a href="/productos">Productos</a>
+        <a href="/nosotros">Nosotros</a>
+        <a href="/contacto">Contacto</a>
+    </nav>
   )
 }
 
--- clase-04/01-react-router/src/pages/Inicio.jsx
+++ clase-04/01-react-router/src/pages/Inicio.jsx
@@ -6,8 +6,11 @@ const Inicio = () => {
         <h1>Inicio</h1>
         <p>Bienvenido a nuestra primera App con router</p>
 
-
-        <p>Vamos a visitar la página de productos: <a href="/productos">Productos</a></p>
+        {/* Para navegación interna entre páginas del proyecto -> Link */}
+        {/* Para navegación externa a páginas fuera del proyecto -> a */}
+        <p>Vamos a visitar la página de productos: <Link to="/productos">Productos</Link></p>
+        <p>Ahora queremos conocer a quienes forman este proyecto: <Link to="/nosotros">Nosotros</Link></p>
+        <p>Ahora visitamos a la página de Google: <a to="https://google.com">Google</a></p>
     </>
   )
 }
